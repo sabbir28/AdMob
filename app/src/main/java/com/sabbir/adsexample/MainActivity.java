046e28102f90f25b3118acb4a42ee832cbc285fa
sabbir28 <sabbirb228@gmail.com>
2024-08-24 11:14:37 +0000

    fix some error

--- app/src/main/java/com/sabbir/adsexample/MainActivity.java
+++ app/src/main/java/com/sabbir/adsexample/MainActivity.java
@@ -5,27 +5,35 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.ads.nativetemplates.NativeTemplateStyle;
 import com.google.android.ads.nativetemplates.TemplateView;
-import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.nativead.NativeAd;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private TemplateView templateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        templateView = findViewById(R.id.my_template);
+
+        new Thread(
+                () -> {
+                    // Initialize the Google Mobile Ads SDK on a background thread.
+                    MobileAds.initialize(this, initializationStatus -> {});
+                })
+                .start();
+
+        TemplateView templateView = findViewById(R.id.my_template);
 
         if (templateView == null) {
             Log.e(TAG, "TemplateView is not found in the layout.");
@@ -33,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         initializeCustomAds();
+
+        TryBannerADS();
+
+
+    }
+
+    private void TryBannerADS() {
+
     }
 
     private void initializeCustomAds() {
@@ -40,8 +56,7 @@ public class MainActivity extends AppCompatActivity {
         AdLoader adLoader = new AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
                 .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
                     @Override
-                    public void onNativeAdLoaded(NativeAd nativeAd) {
-                        ColorDrawable white;
+                    public void onNativeAdLoaded(@NonNull NativeAd nativeAd) {
                         NativeTemplateStyle styles = new
                                 NativeTemplateStyle.Builder().withMainBackgroundColor(new ColorDrawable(Color.WHITE)).build();
                         TemplateView template = findViewById(R.id.my_template);
